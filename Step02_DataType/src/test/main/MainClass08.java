189b6791dae99ab776dc720b0577c20234df7ab0
kimgura <tjalstl@naver.com>
2020-05-28 16:29:24 +0900

    01

---
+++ Step02_DataType/src/test/main/MainClass08.java
@@ -0,0 +1,13 @@
+package test.main;
+
+public class MainClass08 {
+
+	public static void main(String[] args) {
+		String str = "abcde123";
+		int size = str.length();
+		char ch =str.charAt(5);
+		String result = str.toUpperCase();
+
+	}
+
+}
